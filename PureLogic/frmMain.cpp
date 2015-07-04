86db8ca9471f03418311dca4454e73eb184ddf1e
Jucko13 <ricardoderoode@msn.com>
2015-07-04 12:40:33 +0200

    First compile

---
+++ PureLogic/frmMain.cpp
@@ -0,0 +1,46 @@
+#include "frmMain.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace PureLogic;
+
+[STAThread]
+int main(array<String^> ^args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	PureLogic::frmMain form;
+	Application::Run(%form);
+
+	return 0;
+}
+
+
+
+void frmMain::frmMain_Load(System::Object^  sender, System::EventArgs^  e) {
+	//MessageBox::Show("Startup test");
+}
+
+void frmMain::frmMain_Paint(System::Object^  sender, PaintEventArgs^  e) {
+	System::Drawing::Graphics ^g = e->Graphics;
+	
+	Drawing::SolidBrush ^blockBack = gcnew Drawing::SolidBrush(Color::FromArgb(255, 222, 140));
+	Drawing::SolidBrush ^programBackground = gcnew Drawing::SolidBrush(Color::FromArgb(180, 180, 180));
+	Drawing::Pen ^blockBorder = gcnew Pen(Drawing::Color::Black);
+	
+
+	//paint background
+	e->Graphics->FillRectangle(programBackground, frmMain::ClientRectangle);
+
+	
+	
+
+	//draw background of block
+	e->Graphics->FillRectangle(blockBack, 40, 40, 25, 35);
+	
+	e->Graphics->DrawRectangle(blockBorder, 40, 40, 25, 35);
+
+
+
+
+}
---
+++ PureLogic/frmMain.h
@@ -0,0 +1,54 @@
+#pragma once
+
+namespace PureLogic {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for frmMain
+	/// </summary>
+	public ref class frmMain : public System::Windows::Forms::Form
+	{
+	public:
+		frmMain(void){ InitializeComponent(); }
+		void frmMain_Load(Object^  sender, EventArgs^  e);
+		void frmMain_Paint(Object^  sender, PaintEventArgs^  e);
+
+	protected:
+		~frmMain()
+		{
+			if (components){ delete components;	}
+		}
+
+	private:
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout(); //pause the layout refresh
+
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Name = L"frmMain";
+			this->Text = L"frmMain";
+			this->Load += gcnew System::EventHandler(this, &frmMain::frmMain_Load);
+			this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &frmMain::frmMain_Paint);
+
+
+			this->ResumeLayout(false); //resume the refresh
+
+		}
+#pragma endregion
+	};
+}
